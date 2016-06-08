3b72d756739b3957151ed5c28eb02730e7fa0637
Mathias Bynens <mathias@qiwi.be>
2016-06-08 21:02:29 +0200

    Initial commit

---
+++ tests/tests.js
@@ -0,0 +1,15 @@
+import test from 'ava';
+import canonicalProperties from '../index.js';
+
+test(t => {
+	t.true(canonicalProperties.has('ASCII'));
+	t.false(canonicalProperties.has('ascii'));
+	t.true(canonicalProperties.has('Any'));
+	t.false(canonicalProperties.has('any'));
+	t.true(canonicalProperties.has('Assigned'));
+	t.false(canonicalProperties.has('assigned'));
+	t.true(canonicalProperties.has('Block'));
+	t.false(canonicalProperties.has('blk'));
+	t.true(canonicalProperties.has('ISO_Comment'));
+	t.false(canonicalProperties.has('isc'));
+});
